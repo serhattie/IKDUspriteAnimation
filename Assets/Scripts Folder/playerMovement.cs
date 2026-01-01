7d4343642a400a05038a7e815ffb4bd141f64a81
serhattie <serhat.ali.camur@gmail.com>
2026-01-01 21:56:00 +0100

    Kommentarer

--- Assets/Scripts Folder/playerMovement.cs
+++ Assets/Scripts Folder/playerMovement.cs
@@ -1,39 +1,38 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.InputSystem; // needed for the new Input System (WASD / controller)
 
-public class playerMovement : MonoBehaviour
+public class playerMovement : MonoBehaviour // script that controls player movement
 {
+    private Vector2 movement; // stores movement direction from input (x = left/right, y = up/down)
+    private Rigidbody2D myBody; // Rigidbody2D used to move the player with physics
+    private Animator myAnimator; // Animator that controls walking and idle animations
 
-    private Vector2 movement;
-    private Rigidbody2D myBody;
-    private Animator myAnimator;
+    [SerializeField] private int speed; // how fast the player moves (editable in Inspector)
 
-    [SerializeField] private int speed;
-
-    private void Awake()
+    private void Awake() // runs once when the object is created
     {
-        myBody = GetComponent<Rigidbody2D>(); 
-        myAnimator = GetComponent<Animator>();
+        myBody = GetComponent<Rigidbody2D>(); // gets the Rigidbody2D component from the player
+        myAnimator = GetComponent<Animator>(); // gets the Animator component from the player
     }
 
-    private void OnMovement(InputValue value)
+    private void OnMovement(InputValue value) // called by the Input System when movement input happens
     {
-        movement = value.Get<Vector2>();
+        movement = value.Get<Vector2>(); // reads WASD / joystick input and stores it as a Vector2
 
-        if(movement.x !=0 || movement.y != 0)
+        if (movement.x != 0 || movement.y != 0) // checks if the player is moving
         {
-            myAnimator.SetFloat("x", movement.x);
-            myAnimator.SetFloat("y", movement.y);
-            myAnimator.SetBool("isWalking", true);
+            myAnimator.SetFloat("x", movement.x); // sends horizontal direction to Animator
+            myAnimator.SetFloat("y", movement.y); // sends vertical direction to Animator
+            myAnimator.SetBool("isWalking", true); // tells Animator to play walking animation
         }
-        else
+        else // runs when there is no movement input
         {
-            myAnimator.SetBool("isWalking", false);
+            myAnimator.SetBool("isWalking", false); // stops walking animation (idle)
         }
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() // runs at a fixed rate, used for physics updates
     {
-        myBody.linearVelocity = movement * speed;
+        myBody.linearVelocity = movement * speed; // moves the player based on direction and speed
     }
 }
